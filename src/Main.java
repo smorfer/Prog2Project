fb09a5033cec144d476d682da157d9a87ad1fdd0
Samuel Glogger <smorfi98@gmail.com>
2018-03-27 21:07:12 +0200

    Fixed null pointer exception of the EntitySet methods following: -removeEntity (ID was checked without checking for an initialized Entity) -toString (Cant check for Information of uninitialized Objects) -nextSteps (Cant execute nextStep method of uninitialized Objects)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import Entities.beasts.GoodBeast;
+import Entities.squirrels.MasterSquirrel.HandOperatedMasterSquirrel;
+import geom.XY;
+
 public class Main {
     public static void main(String[] args) {
         boolean running = true;
 
+        EntitySet entitySet = new EntitySet();
+        entitySet.addEntity(new HandOperatedMasterSquirrel(0,1000, XY.ORIGIN));
+        entitySet.addEntity(new GoodBeast(1,XY.DOWN_RIGHT));
+        entitySet
         while(running){
-
+            System.out.print(entitySet.toString());
+            entitySet.nextStep();
         }
     }
 }
