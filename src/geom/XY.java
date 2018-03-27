f527ae54769f24b73af2c2fc08198f7262a309cf
Samuel Glogger <smorfi98@gmail.com>
2018-03-27 15:04:40 +0200

    Adding getRandomDirection method in XY class.

--- src/geom/XY.java
+++ src/geom/XY.java
@@ -1,5 +1,7 @@
 package geom;
 
+import java.util.Random;
+
 public final class XY {
     private final int X;
     private final int Y;
@@ -38,5 +40,30 @@ public final class XY {
                 "X=" + X +
                 ", Y=" + Y +
                 '}';
+    }
+
+    public static XY getRandomDirection() {
+        int selector = new Random().nextInt(8);
+        switch (selector)
+        {
+            case 0:
+                return UP;
+            case 1:
+                return DOWN;
+            case 2:
+                return RIGHT;
+            case 3:
+                return LEFT;
+            case 4:
+                return UP_RIGHT;
+            case 5:
+                return UP_LEFT;
+            case 6:
+                return DOWN_RIGHT;
+            case 7:
+                return DOWN_LEFT;
+            default:
+                return ORIGIN;
+        }
     }
 }
