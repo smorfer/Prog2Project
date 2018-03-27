fe54635a399e04b12d7d8f085e2e44db8ccb676c
Samuel Glogger <smorfi98@gmail.com>
2018-03-27 21:14:01 +0200

    Added out/ folder to gitignore and completed forgotten statment in Main class.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Entities.beasts.GoodBeast;
+import Entities.plants.BadPlant;
 import Entities.squirrels.MasterSquirrel.HandOperatedMasterSquirrel;
 import geom.XY;
 
@@ -9,7 +10,7 @@ public class Main {
         EntitySet entitySet = new EntitySet();
         entitySet.addEntity(new HandOperatedMasterSquirrel(0,1000, XY.ORIGIN));
         entitySet.addEntity(new GoodBeast(1,XY.DOWN_RIGHT));
-        entitySet
+        entitySet.addEntity(new BadPlant(2, new XY(10,10)));
         while(running){
             System.out.print(entitySet.toString());
             entitySet.nextStep();
