e44e3bad2fecf40d03d87d847e32c1df7fc8d02d
Luis Schweigard <luis.schweigard@gmail.com>
2018-06-11 14:54:30 +0200

    Luis signing off

--- src/core/BoardConfig.java
+++ src/core/BoardConfig.java
@@ -11,7 +11,7 @@ import java.util.logging.Logger;
 
 public class BoardConfig {
 
-    public BoardConfig(){
+    BoardConfig(){
         if(Files.exists(Paths.get("config.properties"))){
             loadFromFile();
         }
